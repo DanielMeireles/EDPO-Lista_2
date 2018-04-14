fb857e555fe15977451b15e33c7fe423a25074e6
Daniel Meireles <daniel.meireles@outlook.com>
2018-04-13 22:10:21 -0300

    First commit

---
+++ No.js
@@ -0,0 +1,8 @@
+function Ponto(id){
+    this.id = id;
+    this.cidade = "";
+    this.estado = "";
+    this.x = 0;
+    this.y = 0;
+    this.r = 0.5;//Aqui regulamos o tamanho do ponto
+}
---
+++ lista_2.js
@@ -0,0 +1,221 @@
+var img = new Image();
+img.src = 'mapa.jpg';
+
+window.addEventListener("load", init);
+
+var matrizAdjacencia = [];
+var matrizValorada = [];
+var grauNo = [];
+var nos = [];
+var canvas;
+var ctx;
+
+function init(){
+  document.getElementById("btnRun").addEventListener("click", atualizaGrafo);
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  geraNos(); 
+  montaComboBox();
+  atualizaGrafo();
+
+  canvas.addEventListener('click', function(event) {
+    var rect = canvas.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
+    console.log("x: " + x + " y: " + y); 
+  }, false);
+}
+
+function atualizaGrafo(){
+  if(event.preventDefault) event.preventDefault();
+  event.result = false;
+  montaMatrizAdjacencia();
+  montaGrauNo();
+  desenhaGrafo();
+}
+
+function montaComboBox(){
+  var docfragOrigem = document.createDocumentFragment();
+  var docfragDestino = document.createDocumentFragment();
+  console.log(nos.length);
+  for (var i = 0; i < nos.length; i++) {
+      docfragOrigem.appendChild(new Option(nos[i].cidade, i));
+      docfragDestino.appendChild(new Option(nos[i].cidade, i));
+  }
+  var origem = document.getElementById("origem");
+  var destino = document.getElementById("destino");
+  origem.appendChild(docfragOrigem);
+  destino.appendChild(docfragDestino);
+}
+
+function geraLigacao(){
+  var o = document.getElementById("origem").selectedIndex;
+  var d = document.getElementById("destino").selectedIndex;
+  var n = ";"+o+"-"+d;
+  var ta = document.getElementById("txtadj").value;
+  ta = ta+n;
+  document.getElementById("txtadj").value = ta;
+  atualizaGrafo();
+}
+
+function montaMatrizAdjacencia(){
+  var teste = "0-0;1-1;2-2;3-3;4-4;5-5;6-6;7-7;8-8;9-9;10-10;11-11;12-12;13-13;14-14;15-15;16-16;17-17;18-18;19-19;20-20;21-21;22-22;23-23;24-24;25-25;26-26";
+  var ligacoes = teste.concat(document.fadj.txtadj.value);
+  ligacoes = ligacoes.split(";");
+  for(var i in ligacoes){
+    var ligacao = ligacoes[i];
+    var nos = ligacao.split("-");
+    var n1 = nos[0].trim();
+    var n2 = nos[1].trim();
+    if(!matrizAdjacencia[n1]){
+      matrizAdjacencia[n1] = [];
+    }
+    if(!matrizAdjacencia[n2]){
+      matrizAdjacencia[n2] = [];
+    }
+    matrizAdjacencia[n1][n2] = 1;
+    matrizAdjacencia[n2][n1] = 1;
+  }
+  for(var i=0; i<matrizAdjacencia.length;i++ ){
+    if(grauNo[i]===undefined){ grauNo[i] = 0;}
+    if(nos[i]===undefined){
+      nos[i] = new Ponto(i);
+      nos[i].x = 20*(i+1)+150*Math.random();
+      nos[i].y = 20*(i+1)+150*Math.random();
+    }
+    for(var j=0; j<matrizAdjacencia.length;j++ ){
+      matrizAdjacencia[i][j] = matrizAdjacencia[i][j]?matrizAdjacencia[n1][n2]:0;
+    }
+  }
+  imprimeMatrizAdjacencia();    
+}
+
+function montaGrauNo(){
+  for(var i=0; i<matrizAdjacencia.length;i++ ){
+    for(var j=0; j<matrizAdjacencia.length;j++ ){
+      if(matrizAdjacencia[i][j]){
+        grauNo[i]++;
+      }
+    }
+    grauNo[i]-=1;
+  }
+  imprimeGrauNo();
+}
+
+function imprimeMatrizAdjacencia(){
+  var adj = document.getElementById("adj");
+  adj.innerHTML = "";
+  var i, j;
+  var tr, td, th;
+  tr = document.createElement("tr");
+  th = document.createElement("th");
+  tr.appendChild(th);
+  for(i=0;i<matrizAdjacencia.length;i++){
+    th = document.createElement("th");
+    th.innerText = "N"+i;
+    tr.appendChild(th);
+  }
+  adj.appendChild(tr);
+  for(i=0;i<matrizAdjacencia.length;i++){
+    tr = document.createElement("tr");
+    th = document.createElement("th");
+    th.innerHTML = "N"+i;
+    tr.appendChild(th);
+    for(j=0;j<matrizAdjacencia.length;j++){
+      var td = document.createElement("td");
+      td.innerText = matrizAdjacencia[i][j];
+      tr.appendChild(td);
+    }
+    adj.appendChild(tr);
+  }          
+}
+
+function imprimeGrauNo(){
+  var grau = document.getElementById("grau");
+  grau.innerHTML="";
+  for(i=0;i<matrizAdjacencia.length;i++){
+    tr = document.createElement("tr");
+    tr.innerHTML = "<th>N"+i+"</th><td>"+grauNo[i]+"</td>";
+    grau.appendChild(tr);
+  }
+}
+
+function geraMatrizValorada(o, d, v){
+  matrizValorada[o][d]=v;
+}
+
+function limpaMatrizValorada(){
+  for(i=matrizValorada.length;i>=0;i--){
+    matrizValorada.splice(i, 1);
+  }
+  for(i=0;i<27;i++){
+    matrizValorada[i]=[];
+    for(j=0;j<27;j++){
+        matrizValorada[i][j]=0;
+    }
+  }
+}
+
+function desenhaGrafo(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(img, 0, 0);
+  limpaMatrizValorada(); 
+  for(i=0;i<matrizAdjacencia.length;i++){//Desenha as linhas
+    no = nos[i];
+    for(j=0;j<matrizAdjacencia.length;j++){
+      if(matrizAdjacencia[i][j]){
+        outra = nos[j];
+        ctx.lineWidth = 0.4;
+        ctx.beginPath();
+        ctx.moveTo(no.x, no.y);
+        ctx.lineTo(outra.x, outra.y);
+        ctx.closePath();
+        ctx.strokeStyle = "black";
+        ctx.stroke();
+        valorA = 0;
+        valorB = 0;
+        valor = 0;
+        if(no.x > outra.x){
+          valorA = no.x - outra.x;
+        }
+        if(no.x < outra.x){
+          valorA = outra.x - no.x;
+        }
+        if(no.y > outra.y){
+          valorB = no.y - outra.y;
+        }
+        if(no.y < outra.y){
+          valorB = outra.y - no.y;
+        }
+        valor = parseInt(Math.hypot(valorA, valorB));
+        geraMatrizValorada(no.id, outra.id, valor);
+        if(valor > 0){
+          ctx.font="12px arial";
+          ctx.textAlign="center";
+          ctx.fillStyle = "red"; 
+          ctx.fillText(valor,outra.x + (no.x-outra.x)/2,outra.y + (no.y-outra.y)/2);//Coloca o texto nas ligações
+        }
+      }
+    }
+  }
+  console.log(matrizValorada);
+  for (var i = 0; i < nos.length; i++) {//Desenha os nos
+    no = nos[i];
+    ctx.save();
+    ctx.translate(no.x, no.y);
+    ctx.beginPath();
+    ctx.arc(0, 0, no.r, 0, 2*Math.PI, false);
+    ctx.lineWidth = 2;
+    ctx.fill();
+    ctx.stroke();
+    ctx.lineWidth = 1;
+    //ctx.fillText(i,-no.r,no.r+12);//Coloca o texto nos nos
+    ctx.restore();
+  }
+}
+
+function calculoMenorCaminho(){
+  var o = document.getElementById("origem").selectedIndex;
+  var d = document.getElementById("destino").selectedIndex;
+  console.log(o, d);
+}
